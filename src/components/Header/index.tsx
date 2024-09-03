4a1d5b9de42df63ee1f8dfeac1b40798eb628df9
septiannuriyanto <septian.nuryanto@gmail.com>
2024-09-03 14:24:07 +0800

    Fix whole formatting issue

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -20,7 +20,7 @@ const Header = (props: {
   }
   return (
     <header className=" sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-      <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
+      <div className="flex flex-grow items-end justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11 lg:justify-end">
         {/* <div className="flex items-center gap-2 sm:gap-4 lg:block">
 
             <button
@@ -121,7 +121,7 @@ const Header = (props: {
           </form>
         </div>
 
-        <div className="flex items-center gap-3 2xsm:gap-7">
+        <div className="flex items-end gap-3 2xsm:gap-7">
           <ul className="flex items-center gap-2 2xsm:gap-4">
             {/* <!-- Dark Mode Toggler --> */}
             <DarkModeSwitcher />
--- src/pages/Dashboard/Manpower/components/RosterGannt.tsx
+++ src/pages/Dashboard/Manpower/components/RosterGannt.tsx
@@ -4,7 +4,9 @@ import { useEffect } from 'react';
 import { TimelineViews, TimelineMonth, Agenda, ScheduleComponent, ViewsDirective, ViewDirective, ResourcesDirective, ResourceDirective, Inject, Resize, DragAndDrop } from '@syncfusion/ej2-react-schedule';
 import { extend } from '@syncfusion/ej2-base';
 import * as dataSource from './datasource.json';
-import './RosterGannt.css'
+// import './RosterGannt.css'
+// import './bootstrap.min.css'
+import './bootstrap5.css'
 
 /**
  * schedule timeline resource grouping sample
@@ -15,14 +17,14 @@ const RosterGannt = () => {
         extend([], (dataSource as Record<string, any>).resourceData.concat((dataSource as Record<string, any>).timelineResourceData), null, true) as Record<string, any>[];
     const workDays: number[] = [0, 1, 2, 3, 4, 5];
     const projectData: Record<string, any>[] = [
-        { text: 'PROJECT 1', id: 1, color: '#cb6bb2' },
-        { text: 'PROJECT 2', id: 2, color: '#56ca85' },
-        { text: 'PROJECT 3', id: 3, color: '#df5286' }
+        { text: 'GROUP LEADER', id: 1, color: '#cb6bb2' },
+        { text: 'OPERATOR FT', id: 2, color: '#56ca85' },
+        { text: 'FUELMAN', id: 3, color: '#df5286' }
     ];
     const categoryData: Record<string, any>[] = [
-        { text: 'Nancy', id: 1, groupId: 1, color: '#df5286' },
-        { text: 'Steven', id: 2, groupId: 1, color: '#7fa900' },
-        { text: 'Robert', id: 3, groupId: 2, color: '#ea7a57' },
+        { text: 'Septian Nuriyanto', id: 1, groupId: 1, color: '#df5286' },
+        { text: 'Taufiqurrahman', id: 2, groupId: 1, color: '#7fa900' },
+        { text: 'Arif Dwi F', id: 3, groupId: 1, color: '#ea7a57' },
         { text: 'Smith', id: 4, groupId: 2, color: '#5978ee' },
         { text: 'Michael', id: 5, groupId: 3, color: '#df5286' },
         { text: 'Root', id: 6, groupId: 3, color: '#00bdae' }
