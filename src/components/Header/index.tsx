185049b1bc8ffce7baeb7e54cce5a16e519ac2dc
septiannuriyanto <septian.nuryanto@gmail.com>
2025-09-19 08:32:12 +0800

    Fix Z Index sidebar and header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,7 +19,7 @@ const Header = (props: {
   document.querySelector('sidebar-main')?.classList.remove('hidden');
   }
   return (
-    <header className=" sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+    <header className=" sticky top-0 z-99 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-end justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11 lg:justify-end">
         {/* <div className="flex items-center gap-2 sm:gap-4 lg:block">
 
