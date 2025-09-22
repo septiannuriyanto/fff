6f8aad24a1a0d6e7fb8a01afd07bfc0e50d6b369
septiannuriyanto <septian.nuryanto@gmail.com>
2025-09-22 18:17:26 +0800

    fix chunk size

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // naikin batas warning jadi 1MB
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'exceljs': ['exceljs'],
+          'qrcode': ['qrcode'],
+          'lucide-react': ['lucide-react'],
+        },
+      },
+    },
+  },
 })
